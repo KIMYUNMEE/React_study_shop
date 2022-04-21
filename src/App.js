a0bedcfea4a57829772142a5cfae6bb9372026c4
KIMYUNMEE <jayoo8665@naver.com>
2022-04-21 18:05:15 +0900

    React공부 - 쇼핑몰 레이아웃 제작

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { Navbar,Nav,NavDropdown,Form,Button,FormControl,Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container, Button } from 'react-bootstrap';
+import data from './data.js';
 function App() {
+   let [shoes,shoes변경] = useState(data);
   return (
     <div className="App">
  <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">coco-shop</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
@@ -22,7 +24,35 @@ function App() {
       </Nav>
     </Navbar.Collapse>
   </Container>
-</Navbar>
+      </Navbar>
+      <div className="background">
+        <div>
+        <h1>
+          20% Season off
+        </h1>
+        <p>asdopksadkwklrknkffkjaskfakflask'fdwpd</p>
+         <Button variant="primary">Learn More</Button>{' '}
+      </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h2>상품명</h2>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h2>상품명</h2>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h2>상품명</h2>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
