fa8d3b855eaff2b1549c15a68191246675d09b6f
KIMYUNMEE <jayoo8665@naver.com>
2022-05-03 23:28:28 +0900

    React공부 - 자료의 순서가 변경되면 상세페이지도 고장나는 문제->현재 shoes라는 상품데이터들 안엔 {id : 0} 이런 영구번호가 있는데 현재 /:id 자리에 입력한 값과 영구번호가 같은 {상품데이터}를 찾아서 데이터바인딩해주기 es6신문법 find()메소드 이용하기

--- src/Detail.js
+++ src/Detail.js
@@ -2,7 +2,11 @@ import React,{useState} from 'react';
 import { useHistory,useParams } from 'react-router-dom';
 
 function Detail(props){
-    let { id } = useParams();
+
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function(상품){
+    return 상품.id == id
+  });
   return (
     <div className="container">
       <div className="row">
@@ -10,9 +14,9 @@ function Detail(props){
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-         <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p>
+         <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
