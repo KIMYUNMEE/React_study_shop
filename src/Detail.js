1db71ccb7b5feb37fabf5735be956401899d0638
KIMYUNMEE <jayoo8665@naver.com>
2022-05-14 10:40:26 +0900

    React공부 - props없이 state를 공유할 수있는 redux설치

--- src/Detail.js
+++ src/Detail.js
@@ -5,13 +5,13 @@ function Detail(props){
 
   let { id } = useParams();
   let 찾은상품 = props.shoes.find(function(상품){
-    return 상품.id == id
+    return 상품.id === id
   });
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt="상품"/>
         </div>
         <div className="col-md-6 mt-4">
          <h4 className="pt-5">{찾은상품.title}</h4>
